1e2c8e6752fb83cbbaa03c22a2ebb7798e750c04
Juho Friman <juho.t.friman@gmail.com>
2016-03-04 21:30:36 +0200

    Initial Docker setup

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /opt/xmltv
+mkdir data
+echo "Hang tight... generating..."
+tv_grab_fi --config-file /opt/xmltv/tv_grab_fi.conf --days 1 --quiet > data/tvdata.xml
+echo "Done!"
+cd data
+python -m SimpleHTTPServer 8000
