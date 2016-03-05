1b232b987332448d6b96b3eca5aded5c090fc66e
Juho Friman <juho.t.friman@gmail.com>
2016-03-05 20:59:56 +0200

    Intial timatable proto

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -9,6 +9,62 @@ var ReactDOM = require('react-dom')
 var update = require('react-addons-update');
 var classNames = require('classnames');
 
+var Program = React.createClass({
+  render: function() {
+    var programHeigh = { height: this.props.program.length };
+    return (
+        <div className="program" style={programHeigh}>{this.props.program.name}</div>
+    );
+  }
+});
+
+var Schedule = React.createClass({
+  render: function() {
+    return (
+      <div className="schedule-container">
+        <div className="schedule">
+          {this.props.programs.map(function(program, i){
+            return <Program program={program} key={i} />;
+          })}
+        </div>
+      </div>
+    );
+  }
+});
+
+var Timeline = React.createClass({
+  getTimelineStyling: function() {
+    // Here we should get current time and scale by that
+    return {
+      pastStyle: { height: 300 },
+      futureStyle: { height: 100 }
+    };
+  },
+  render: function() {
+    var timelineStyling = this.getTimelineStyling();
+    return (
+      <div className="timeline">
+        <div className="block hours">
+          <div className="point">13:00</div>
+          <div className="point">14:00</div>
+          <div className="point">15:00</div>
+          <div className="point">16:00</div>
+          <div className="point">17:00</div>
+        </div>
+        <div className="block">
+          {this.props.channels.map(function(channel, i){
+            return <Schedule programs={channel} key={i} />;
+          })}
+          <div className="past" style={timelineStyling.pastStyle}></div>
+          <div className="now-cue"></div>
+          <div className="future" style={timelineStyling.futureStyle}></div>
+
+        </div>
+      </div>
+    );
+  }
+});
+
 var ChannelToggle = React.createClass({
   toggle: function(e) {
     this.props.toggleCallback(this.props.channel);
@@ -44,10 +100,44 @@ var NoTV = React.createClass({
   getInitialState: function() {
     return {
       channels: [
-        {'name': 'Kanava A', 'toggled': false},
-        {'name': 'Kanava B', 'toggled': false},
-        {'name': 'Kanava C', 'toggled': false},
-        {'name': 'Kanava D', 'toggled': false}]};
+        {'name': 'Kanava A',
+         'toggled': false,
+         'programs': [
+           { length: 20, name: 'Uutiset'},
+           { length: 100, name: 'Haluatko miljonääriksi?'},
+           { length: 50, name: 'Reinikainen'},
+           { length: 30, name: 'Tankki täyteen'},
+           { length: 60, name: 'Rekkakuskit lesboina'},
+           { length: 200, name: 'Eduskunnan täysistunto'}
+         ]},
+         {'name': 'Kanava B',
+          'toggled': false,
+          'programs': [
+            { length: 100, name: 'James Potkukelkka -show'},
+            { length: 150, name: 'Hullut siivoojat'},
+            { length: 50, name: 'Kauniit ja rohkeat'},
+            { length: 10, name: 'Mainos-tv'},
+            { length: 80, name: 'Pahkasika'},
+            { length: 50, name: 'Jari Sarasvuo'}
+          ]},
+          {'name': 'Kanava C',
+           'toggled': false,
+           'programs': [
+             { length: 100, name: 'Hevoshullu'},
+             { length: 300, name: 'Olympialaiset'},
+             { length: 50, name: 'Elokuva: Keisarikunta'},
+             { length: 10, name: 'Kotkalaisen lauluperinteen perässä'},
+             { length: 80, name: 'Jeppistä'},
+             { length: 10, name: 'Keno!'}
+           ]},
+           {'name': 'Kanava D',
+            'toggled': false,
+            'programs': [
+              { length: 100, name: 'Ryhmä-X'},
+              { length: 150, name: 'Mönkiäisohjelmaa'},
+              { length: 50, name: 'Venäläiset suolakurkut'}
+            ]},
+       ]};
   },
   toggleChannel: function(channel) {
     this.setState(
@@ -56,12 +146,16 @@ var NoTV = React.createClass({
           { '$apply': function(ov) {
             return ov.map(function(c) {
               if(c.name === channel.name) {
-                return { 'name': c.name, 'toggled': !c.toggled };
+                return { 'name': c.name, 'toggled': !c.toggled, 'programs': c.programs };
               } else {
                 return c;
               }
             });
           }}}));
+  },
+  getProgramArrays: function() {
+    var a = this.state.channels.filter(function(c) { return c.toggled; }).map(function(c) { return c.programs; })
+    return a;
   },
   render: function() {
     return (
@@ -70,7 +164,12 @@ var NoTV = React.createClass({
           <input type="text"/>
         </div>
         <div>
-          <ChannelSelector channels={this.state.channels} toggleCallback={this.toggleChannel}/>
+          <div className="channel-listing-float">
+            <ChannelSelector channels={this.state.channels} toggleCallback={this.toggleChannel}/>
+          </div>
+          <div className="timeline-float">
+            <Timeline channels={this.getProgramArrays()}/>
+          </div>
         </div>
       </div>
     );
