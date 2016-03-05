42fc4f3cbedb734dac93674fbf0e6c5695df6c85
Juho Friman <juho.t.friman@gmail.com>
2016-03-05 15:32:17 +0200

    Now user can toggle channels in model

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -6,22 +6,72 @@ require("../styles/notv.scss")
 
 var React = require('react')
 var ReactDOM = require('react-dom')
+var update = require('react-addons-update');
+var classNames = require('classnames');
+
+var ChannelToggle = React.createClass({
+  toggle: function(e) {
+    this.props.toggleCallback(this.props.channel);
+  },
+  render: function() {
+    var classes = classNames('channel-toggle', {
+        'toggled': this.props.channel.toggled
+    });
+    return (
+      <div className={classes}>
+        <div onClick={this.toggle} className="name">{this.props.channel.name}</div>
+      </div>
+    );
+  }
+});
 
 var ChannelSelector = React.createClass({
   render: function() {
+    var toggleCallback = this.props.toggleCallback;
     return(
       <div>
-        <h2>Valitse kanavat</h2>
+        <div className="channel-listing">
+          {this.props.channels.map(function(channel, i){
+            return <ChannelToggle channel={channel} toggleCallback={toggleCallback} key={i} />;
+          })}
+        </div>
       </div>
     );
   }
 });
 
 var NoTV = React.createClass({
+  getInitialState: function() {
+    return {
+      channels: [
+        {'name': 'Kanava A', 'toggled': false},
+        {'name': 'Kanava B', 'toggled': false},
+        {'name': 'Kanava C', 'toggled': false},
+        {'name': 'Kanava D', 'toggled': false}]};
+  },
+  toggleChannel: function(channel) {
+    this.setState(
+      update(this.state,
+        {'channels':
+          { '$apply': function(ov) {
+            return ov.map(function(c) {
+              if(c.name === channel.name) {
+                return { 'name': c.name, 'toggled': !c.toggled };
+              } else {
+                return c;
+              }
+            });
+          }}}));
+  },
   render: function() {
     return (
       <div className="notv-container">
-        <ChannelSelector></ChannelSelector>
+        <div>
+          <input type="text"/>
+        </div>
+        <div>
+          <ChannelSelector channels={this.state.channels} toggleCallback={this.toggleChannel}/>
+        </div>
       </div>
     );
   }
