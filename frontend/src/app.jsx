8f528be2685e209bd60d610ab10bb57baba4a894
Juho Friman <juho.t.friman@gmail.com>
2016-03-05 11:47:59 +0200

    Simple prototyping for react front-end.
    Using gulp, webpack and webpack-html-plugin.
    
    Run gulp for watching sources and serve ./dist. I'm using:
    
        python -m SimpleHTTPServer 8000

---
+++ frontend/src/app.jsx
@@ -0,0 +1,9 @@
+/** @jsx React.DOM */
+'use strict'
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+ReactDOM.render(
+  <div>Here's Johnny!</div>,
+  document.getElementById('notv-react')
+);
