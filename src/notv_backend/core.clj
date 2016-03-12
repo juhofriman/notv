b159fa53de89e18dafb837a62bc7732aa2cbc99a
Juho Friman <juho.t.friman@gmail.com>
2016-03-12 15:06:34 +0200

    Created really simple backend for listing channels

--- src/notv_backend/core.clj
+++ src/notv_backend/core.clj
@@ -3,13 +3,31 @@
   (:require [compojure.api.sweet :refer :all]
             [compojure.route :as route]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]))
-            
-(defroutes static
-  (route/resources "/"))
+            [schema.core :as s]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]))
+
+(defn zip-str [s]
+  (zip/xml-zip
+    (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+
+(defn xml-channel-to-channel [{attrs :attrs content :content}]
+  {:id (:id attrs)
+   :name (first (:content (first content)))})
+
+
+(defn get-channels [xmlstr]
+  (let [channels (filter #(= (:tag %) :channel) (clojure.zip/children (zip-str xmlstr)))]
+    (map xml-channel-to-channel channels)))
+
+(defn get-channels-foo []
+  (-> (slurp "resources/example-tvdata.xml")
+      get-channels))
 
 (defapi app
-  static
-  (GET "/hello" []
-    :query-params [name :- String]
-    (ok {:message (str "Hello, " name)})))
+  (undocumented
+    (route/resources "/")
+    (GET "/channels" [] (ok (get-channels-foo)))
+    (GET "/hello" []
+      :query-params [name :- String]
+      (ok {:message (str "Hello, " name)}))))
--- test/notv_backend/core_test.clj
+++ test/notv_backend/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [notv-backend.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def trivial-xml
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+   <!DOCTYPE tv SYSTEM \"xmltv.dtd\">
+   <tv source-info-url=\"multiple\" source-data-url=\"multiple\" generator-info-name=\"XMLTV\" generator-info-url=\"http://xmltv.org/\">
+   </tv>")
+
+(def non-trivial-xml
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+   <!DOCTYPE tv SYSTEM \"xmltv.dtd\">
+   <tv source-info-url=\"multiple\" source-data-url=\"multiple\" generator-info-name=\"XMLTV\" generator-info-url=\"http://xmltv.org/\">
+     <channel id=\"channel-1\">
+       <display-name lang=\"fi\">CHANNEL-1</display-name>
+     </channel>
+     <channel id=\"channel-2\">
+       <display-name lang=\"fi\">CHANNEL-2</display-name>
+     </channel>
+   </tv>")
+
+(deftest xml-parsing-test
+  (testing "Parsing channels from xml"
+    (is (= (count (get-channels trivial-xml)) 0))
+    (is (= (count (get-channels non-trivial-xml)) 2))
+    (is (= (:id (first (get-channels non-trivial-xml))) "channel-1"))
+    (is (= (:name (first (get-channels non-trivial-xml))) "CHANNEL-1"))
+    (is (= (:id (second (get-channels non-trivial-xml))) "channel-2"))
+    (is (= (:name (second (get-channels non-trivial-xml))) "CHANNEL-2"))))
