5de4181fd9e0253bdbd516d33c6d245b354ab11e
Juho Friman <juho.t.friman@gmail.com>
2016-04-08 20:26:13 +0300

    Minor tweaks for dev scripts

--- bin/run-local.sh
+++ bin/run-local.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-docker run -p 3000:3000 --volumes-from xmltv --name notv juhofriman/notv
+docker stop notv
+docker rm notv
+
+NREPL_PORT=7888
+docker run -p 3000:3000 -p 7888:7888 --volumes-from xmltv --name notv juhofriman/notv
